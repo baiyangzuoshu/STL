6d385604617769952cb1533f8bc48ee33a5a16f9
youjunmao <942357585@qq.com>
2017-10-20 22:26:40 +0800

    stack操练

---
+++ day01/day01/stack操练.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+#include"stack"
+
+void main51()
+{
+	stack<int> s;
+
+	for (int i = 0; i < 10; i++)
+	{
+		s.push(i+100);
+	}
+
+	cout << "size:" << s.size() << endl;
+
+	while (!s.empty())
+	{
+		int data = s.top();
+		cout << data << " ";
+		s.pop();
+	}
+}
+
+class Teacher
+{
+public:
+	int age;
+	char  name[32];
+	void printTeacherInfo()
+	{
+		cout << " age:" << age;
+	}
+};
+
+void main52()
+{
+	Teacher t1, t2, t3;
+	t1.age = 32;
+	t2.age = 34;
+	t3.age = 36;
+
+	stack<Teacher*> s;
+	s.push(&t1);
+	s.push(&t2);
+	s.push(&t3);
+
+	while (!s.empty())
+	{
+		Teacher* t = s.top();
+		t->printTeacherInfo();
+		s.pop();
+	}
+
+}
+int main()
+{
+	//main51();
+	main52();
+	cout << "helloword" << endl;
+	system("pause");
+	return 0;
+}
